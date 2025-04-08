ec155f9b5904656b42404d96c878895abd1c5428
Nguyễn Phúc Toàn <ngphuc_toan@tutanota.com>
2025-04-08 08:19:10 +0700

    Add manifest.json

---
+++ frontend/sw.js
@@ -0,0 +1,12 @@
+const VERSION = "v1";
+const CACHE_NAME = `todo-app-${VERSION}`;
+
+const APP_ASSETS = [
+    "/",
+    "/index.html",
+    "/js/main.js",
+    "/assets/bg.jpg",
+    "/assets/empty.jpg",
+    "/assets/icons/192.png",
+    "/assets/icons/512.png"
+];
